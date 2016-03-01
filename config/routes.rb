81782f43fed32f227c0493ef2d7c0c3b0353a409
Jose Carlos <sipox11@gmail.com>
2016-03-01 10:58:19 +0100

    Add route for dig ocean omniauth callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :products
   root 'products#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
